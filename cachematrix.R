a41a1ef972d47327af9a4b2e8f5066138529ddd3
LI Xiaoxu <lixiaoxu06@gmail.com>
2015-03-22 10:33:06 +0800

    Matrix inverse solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matix inversion is usually a costly computation and there may be some benifit to caching the
+## inverse of a matrix rather than compute it repeatedly.
 
-## Write a short comment describing this function
+## makeCacheMatrix, this function creates a special "matrix" object that can cache its inverse.
+## the spcial "matrix" object has a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of inverse of the matrix
+## 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    sol <- NULL
+    
+    set <- function(y){
+        x <<- y
+        sol <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) sol <<- inverse
+    getinverse <- function() sol
+    
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve, this function computes the inverse of the spcial matrix returned by makeCacheMatrix
+## above. If the inverse has already been calculated (and the matrix has not changed), then the 
+## cacheSolve shoud retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    sol <- x$getinverse()
+    
+    if(!is.null(sol)){
+        message("getting cached data.")
+        return(sol)
+    }
+    
+    data <- x$get()
+    sol <- solve(data)
+    x$setinverse(sol)
+    sol
 }
+
+## Example:
+## X <- cbind(c(0, 1, 2), c(1, 1, -1), c(2, 4, 0))
+#
+## Creates a "matrix" object.
+## M <- makeCacheMatrix(X)
+## M$get()
+##
+## console outputs:
+##      [,1] [,2] [,3]
+## [1,]    0    1    2
+## [2,]    1    1    4
+## [3,]    2   -1    0
+##
+## Computing inverse of the matrix for the first time.
+## cacheSolve(M)
+##
+## console outputs:
+##      [,1] [,2] [,3]
+## [1,]  2.0   -1  1.0
+## [2,]  4.0   -2  1.0
+## [3,] -1.5    1 -0.5
+##
+## Retrieving the inverse from the cache for the second time.
+## cacheSolve(M)
+##
+## console outputs:
+## getting cached data.
+## [,1] [,2] [,3]
+## [1,]  2.0   -1  1.0
+## [2,]  4.0   -2  1.0
+## [3,] -1.5    1 -0.5
